91c185a5220865e74478038ee9a98cce41057cde
ZhenShuo2021 <98386542+ZhenShuo2021@users.noreply.github.com>
2025-03-10 23:07:00 +0800

    simplify portfolio page

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,14 +1,17 @@
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./styles.module.css";
 
-export function Hero() {
+export function Hero({ title, tagline, className }) {
   const { siteConfig } = useDocusaurusContext();
+  const heroTitle = title || siteConfig.title;
+  const heroTagline = tagline || siteConfig.tagline;
+
   return (
-    <header>
+    <header className={className || styles.hero}>
       <section className={styles.herowave}>
         <div className="content">
-          <h2 className={styles.h2}>{siteConfig.title}</h2>
-          <h3 className={styles.h3}>{siteConfig.tagline}</h3>
+          <h2 className={styles.h2}>{heroTitle}</h2>
+          <h3 className={styles.h3}>{heroTagline}</h3>
           <br />
         </div>
         <div className={styles.waves}></div>
